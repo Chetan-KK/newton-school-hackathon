fea6a14b12243fa7042be3c267f54d9191194097
Chetan-KK <chetankhulage350@gmail.com>
2024-11-02 00:37:06 +0530

    feat: max width wrapper added

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { ModeToggle } from "./ui/mode-toggle";
+import MaxWidthWrapper from "./max-width-wrapper";
 
 type Props = {};
 
 const Header = (props: Props) => {
   return (
-    <header className="p-5 flex justify-between items-center">
-      <div>next-src-ts-tailwind-shadcn</div>
-      <ModeToggle />
+    <header className="p-5 border-b border-border">
+      <MaxWidthWrapper className="flex justify-between items-center">
+        <div>next-src-ts-tailwind-shadcn</div>
+        <ModeToggle />
+      </MaxWidthWrapper>
     </header>
   );
 };
