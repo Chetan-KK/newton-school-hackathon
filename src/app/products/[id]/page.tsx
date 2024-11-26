4ae71915053fb92f3eb4e342e431b4c54a1a8e4f
Chetan-KK <chetankhulage350@gmail.com>
2024-11-26 17:05:49 +0530

    fix

---
+++ src/app/products/[id]/page.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import Header from "@/components/header";
+import MaxWidthWrapper from "@/components/max-width-wrapper";
+import SectionHeading from "@/components/SectionHeading";
+import { useParams } from "next/navigation";
+import React from "react";
+import { products } from "../../../../products";
+import Image from "next/image";
+
+type Props = {};
+
+const Product = (props: Props) => {
+  const { id } = useParams();
+
+  const product = products.find(
+    (product) => product.id === parseInt(id as string)
+  );
+
+  console.log(product);
+
+  return (
+    <div>
+      <Header />
+      <div className="fixed -z-30 bg-gradient-to-b from-primary/30 to-background h-screen w-screen left-0 top-0"></div>
+      <MaxWidthWrapper className="p-5">
+        <SectionHeading
+          className="!text-left p-5 my-5"
+          heading={product?.name}
+          subHeading={product?.description}
+        />
+        <Image
+          height={500}
+          width={500}
+          className="bg-red-200 border border-border rounded-lg"
+          src={product?.image}
+          alt="image"
+        />
+        <h3 className="text-3xl font-bold mt-5">
+          Price: <span className="text-primary">{product?.price}$</span>
+        </h3>
+      </MaxWidthWrapper>
+    </div>
+  );
+};
+
+export default Product;
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,7 +1,11 @@
 import Header from "@/components/header";
 import MaxWidthWrapper from "@/components/max-width-wrapper";
 import SectionHeading from "@/components/SectionHeading";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { products } from "../../../products";
 
 type Props = {};
 
@@ -17,6 +21,35 @@ const Produts = (props: Props) => {
           heading={"Explore products"}
           subHeading="find your eyemate here ;)"
         />
+        <div className="flex p-5 flex-wrap gap-5 justify-center items-center">
+          {products.map((product) => (
+            <Link
+              href={`/products/${product.id}`}
+              key={product.id}
+              className="w-80 p-5 bg-background/70 rounded-lg border border-border flex flex-col gap-2"
+            >
+              <Image
+                height={500}
+                width={500}
+                className="rounded-lg object-cover"
+                src={product.image}
+                alt={product.name}
+              />
+              <h1 className="text-xl font-bold">{product.name}</h1>
+              <p>{product.description}</p>
+              <div className="grid grid-cols-2 gap-3">
+                <Link href="/products" className="w-full">
+                  <Button className="w-full">Add to Cart</Button>
+                </Link>
+                <Link href={"/"} className="w-full">
+                  <Button variant={"outline"} className="w-full">
+                    View Details
+                  </Button>
+                </Link>
+              </div>
+            </Link>
+          ))}
+        </div>
       </MaxWidthWrapper>
     </div>
   );
